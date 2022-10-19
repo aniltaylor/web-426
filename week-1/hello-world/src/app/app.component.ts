39a095de912a745c43437c552d29bccd07600561
aprilyanggarwood <yangyyj822@hotmail.com>
2022-10-18 21:21:56 -0700

    added code attributions in files

--- week-1/hello-world/src/app/app.component.ts
+++ week-1/hello-world/src/app/app.component.ts
@@ -1,3 +1,12 @@
+/**
+ * Title: app.component.ts
+ * Author: Professor Krasso
+ * Modified By: April Yang
+ * Date: 10/18/2022
+ * Description: Exercise 1.3 - Hello World component
+ */
+
+
 import { Component } from '@angular/core';
 
 @Component({
--- week-1/hello-world/src/app/app.module.ts
+++ week-1/hello-world/src/app/app.module.ts
@@ -1,3 +1,12 @@
+/*
+ Title: app.module.ts
+ Author: Professor Krasso
+ Modified By: April Yang
+ Date: 10/18/2022
+ Description: Hello World Module Declarations
+*/
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
