43ad9324030f5722698e95c31ed76082c7a32b10
aprilyanggarwood <yangyyj822@hotmail.com>
2022-11-03 01:41:51 -0700

    secure-profile-app initial commit

---
+++ week-3/secure-profile-app/src/app/my-image/my-image.component.ts
@@ -0,0 +1,24 @@
+/*
+   Title: my-image.component.ts
+ Author: Professor Krasso
+ Modified By: April Yang
+ Date: 10/26/2022
+ Description: My image view
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-image',
+  templateUrl: './my-image.component.html',
+  styleUrls: ['./my-image.component.css']
+})
+export class MyImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
